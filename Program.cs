bf3d6a21f31bf295439e18f6981318cadf5d62ec
sabriNoor <nooraosama63@gmail.com>
2025-08-25 12:48:25 +0300

    feat: add setup for the project structure

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using ReviewSharp.Interfaces;
+using ReviewSharp.Services;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Register code review services and parser
+builder.Services.AddScoped<ICodeParserService, CodeParserService>();
+builder.Services.AddScoped<ICodeReviewOrchestratorService, CodeReviewOrchestratorService>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/CodeReview/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=CodeReview}/{action=Upload}/{id?}")
+    .WithStaticAssets();
+
+
+// Add route for CodeReview
+app.MapControllerRoute(
+    name: "codeReview",
+    pattern: "CodeReview/{action=Upload}/{id?}");
+
+app.Run();
