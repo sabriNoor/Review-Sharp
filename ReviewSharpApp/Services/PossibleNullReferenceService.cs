2368f78178b6c172d099a2215dfb1d64d5326340
mohammadsofan <nassersofan@gmail.com>
2025-08-28 19:12:46 +0300

    feat: Exclude nullable variables from possible null reference warnings when checked for null
    - Suppress warnings for nullable fields, properties, locals, and parameters if a null check (e.g., if (p != null) or if (p is not null)) is present before usage.
    - Improves accuracy of PossibleNullReferenceService and reduces false positives.

--- ReviewSharpApp/Services/PossibleNullReferenceService.cs
+++ ReviewSharpApp/Services/PossibleNullReferenceService.cs
@@ -1,3 +1,4 @@
+using Microsoft.CodeAnalysis.CSharp;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -15,28 +16,56 @@ namespace ReviewSharp.Services
                 return results;
 
             var allNullableNames = CollectNullableNames(root);
+            var checkedNames = CollectNullCheckedNames(root);
+
             var memberAccesses = root.DescendantNodes().OfType<MemberAccessExpressionSyntax>().ToList();
-            results.AddRange(CheckMemberAccessesForNull(memberAccesses, allNullableNames));
+            results.AddRange(CheckMemberAccessesForNull(memberAccesses, allNullableNames, checkedNames));
 
             // Check for nullable variables used in binary expressions, assignments, and returns
             var binaryExpressions = root.DescendantNodes().OfType<BinaryExpressionSyntax>();
-            results.AddRange(CheckNullableUsageInExpressions(binaryExpressions, allNullableNames));
+            results.AddRange(CheckNullableUsageInExpressions(binaryExpressions, allNullableNames, checkedNames));
 
             var assignments = root.DescendantNodes().OfType<AssignmentExpressionSyntax>();
-            results.AddRange(CheckNullableUsageInAssignments(assignments, allNullableNames));
+            results.AddRange(CheckNullableUsageInAssignments(assignments, allNullableNames, checkedNames));
 
             var returns = root.DescendantNodes().OfType<ReturnStatementSyntax>();
-            results.AddRange(CheckNullableUsageInReturns(returns, allNullableNames));
+            results.AddRange(CheckNullableUsageInReturns(returns, allNullableNames, checkedNames));
 
             return results;
         }
-        private static IEnumerable<CodeReviewResult> CheckNullableUsageInExpressions(IEnumerable<BinaryExpressionSyntax> expressions, HashSet<string> nullableNames)
+        private static HashSet<string> CollectNullCheckedNames(CompilationUnitSyntax root)
+        {
+            var checkedNames = new HashSet<string>();
+            var ifStatements = root.DescendantNodes().OfType<IfStatementSyntax>();
+            foreach (var ifStmt in ifStatements)
+            {
+                // if (p != null) or if (p is not null)
+                if (ifStmt.Condition is BinaryExpressionSyntax binaryExpr)
+                {
+                    if (binaryExpr.Kind() == SyntaxKind.NotEqualsExpression)
+                    {
+                        if (binaryExpr.Left is IdentifierNameSyntax id)
+                            checkedNames.Add(id.Identifier.Text);
+                    }
+                }
+                if (ifStmt.Condition is IsPatternExpressionSyntax isPattern)
+                {
+                    if (isPattern.Expression is IdentifierNameSyntax id && isPattern.Pattern is UnaryPatternSyntax unary && unary.Pattern is ConstantPatternSyntax constPattern)
+                    {
+                        if (constPattern.Expression is LiteralExpressionSyntax lit && lit.Token.ValueText == "null")
+                            checkedNames.Add(id.Identifier.Text);
+                    }
+                }
+            }
+            return checkedNames;
+        }
+        private static IEnumerable<CodeReviewResult> CheckNullableUsageInExpressions(IEnumerable<BinaryExpressionSyntax> expressions, HashSet<string> nullableNames, HashSet<string> checkedNames)
         {
             foreach (var expr in expressions)
             {
                 foreach (var side in new[] { expr.Left, expr.Right })
                 {
-                    if (side is IdentifierNameSyntax id && nullableNames.Contains(id.Identifier.Text))
+                    if (side is IdentifierNameSyntax id && nullableNames.Contains(id.Identifier.Text) && !checkedNames.Contains(id.Identifier.Text))
                     {
                         var lineNumber = expr.GetLocation().GetLineSpan().StartLinePosition.Line + 1;
                         yield return new CodeReviewResult
@@ -51,11 +80,11 @@ namespace ReviewSharp.Services
             }
         }
 
-        private static IEnumerable<CodeReviewResult> CheckNullableUsageInAssignments(IEnumerable<AssignmentExpressionSyntax> assignments, HashSet<string> nullableNames)
+        private static IEnumerable<CodeReviewResult> CheckNullableUsageInAssignments(IEnumerable<AssignmentExpressionSyntax> assignments, HashSet<string> nullableNames, HashSet<string> checkedNames)
         {
             foreach (var assign in assignments)
             {
-                if (assign.Right is IdentifierNameSyntax id && nullableNames.Contains(id.Identifier.Text))
+                if (assign.Right is IdentifierNameSyntax id && nullableNames.Contains(id.Identifier.Text) && !checkedNames.Contains(id.Identifier.Text))
                 {
                     var lineNumber = assign.GetLocation().GetLineSpan().StartLinePosition.Line + 1;
                     yield return new CodeReviewResult
@@ -69,11 +98,11 @@ namespace ReviewSharp.Services
             }
         }
 
-        private static IEnumerable<CodeReviewResult> CheckNullableUsageInReturns(IEnumerable<ReturnStatementSyntax> returns, HashSet<string> nullableNames)
+        private static IEnumerable<CodeReviewResult> CheckNullableUsageInReturns(IEnumerable<ReturnStatementSyntax> returns, HashSet<string> nullableNames, HashSet<string> checkedNames)
         {
             foreach (var ret in returns)
             {
-                if (ret.Expression is IdentifierNameSyntax id && nullableNames.Contains(id.Identifier.Text))
+                if (ret.Expression is IdentifierNameSyntax id && nullableNames.Contains(id.Identifier.Text) && !checkedNames.Contains(id.Identifier.Text))
                 {
                     var lineNumber = ret.GetLocation().GetLineSpan().StartLinePosition.Line + 1;
                     yield return new CodeReviewResult
@@ -119,13 +148,13 @@ namespace ReviewSharp.Services
                 .Concat(nullableParameters));
         }
 
-        private static IEnumerable<CodeReviewResult> CheckMemberAccessesForNull(IEnumerable<MemberAccessExpressionSyntax> memberAccesses, HashSet<string> nullableNames)
+        private static IEnumerable<CodeReviewResult> CheckMemberAccessesForNull(IEnumerable<MemberAccessExpressionSyntax> memberAccesses, HashSet<string> nullableNames, HashSet<string> checkedNames)
         {
             foreach (var memberAccess in memberAccesses)
             {
                 if (memberAccess.Expression is IdentifierNameSyntax id)
                 {
-                    if (nullableNames.Contains(id.Identifier.Text))
+                    if (nullableNames.Contains(id.Identifier.Text) && !checkedNames.Contains(id.Identifier.Text))
                     {
                         var lineNumber = memberAccess.GetLocation().GetLineSpan().StartLinePosition.Line + 1;
                         yield return new CodeReviewResult
